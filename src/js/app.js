f17e0d9fb39330aa4f0ad56309cc4f7ee3860762
Aurora Jaime <aurora-jaime@gmail.com>
2023-08-25 11:59:14 -0600

    primi

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+//console.log("Holi mundo");
+
+//Declarar una variable
+let nombre;
+//alert("Hola")
+
+
+//nombre = prompt("Ingresa tu nombre:");
+//alert("Hola"+ nombre);
+nombre = "Aurora"
+console.log("Tipo de nombre es: " + typeof(nombre));
+
+let edad = 22;
+console.log("Tipo de edad es: " + typeof(edad));
+
+let sueldo = 50000;
+console.log("Tipo de sueldo es: " + typeof(sueldo));
+
+const pi = 3.1416;
+console.log("Tipo de pi es: " + typeof(sueldo));
+ let dos = 2 
+ let tres = 3
+ let suma = dos + tres
+
+console.log(nombre + edad)
+console.log("La suma es:", suma)
+
---
+++ src/js/primitivos.js
@@ -0,0 +1,24 @@
+let dirMeroria = 0xffff;
+console.log(dirMeroria);
+
+let octal = 0o127;
+console.log(octal);
+
+
+let bits = 0b1010;
+console.log(bits)
+
+let fraccion =.25;
+console.log(fraccion);
+
+
+let c =3e8; //3x10^8 #notacion cientifica
+console.log(c);
+
+let raiz_de_dos = Math.sqrt(2.0);
+console.log(raiz_de_dos);
+
+console.log(10/2);
+
+console.log(hola == falso);
+console.log("" == falso);

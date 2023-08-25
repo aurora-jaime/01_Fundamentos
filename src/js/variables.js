7ffc69c439a35c2db4c338ff88ab277e4f042d57
Aurora Jaime <aurora-jaime@gmail.com>
2023-08-25 12:23:16 -0600

    convertir datos

--- src/js/variables.js
+++ src/js/variables.js
@@ -1,1 +1,24 @@
-let n1 = 1;
+//var alert = 'Alerta';
+//Mejor declarar con Let 
+
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre)
+
+nombre = 'tia_May';
+
+nombre = prompt('Introduce tu nombre:');
+console.log(`Holaaaa ,${nombre}`);
+
+//En javascript se pueden convertir cualquier tipo de datos 
+//clg + tab = console.log()
+
+//Conversiones tipos de datos ____________________________
+
+let x = undefined;
+console.log('Como String:'+ x);
+console.log('Como Number:'+ (x*1));
+console.log('Como Boolean:'+((x)? true:false));
+
